d315575bc268599d3e47df456d787647efbeade9
LuongAng <luongngocanh18102@gmail.com>
2023-10-23 22:20:33 +0700

    chon anh, zoom 1/2 anh

--- chinhanh.py
+++ chinhanh.py
@@ -6,3 +6,26 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import Label, Canvas, Button, ttk
 from PIL import Image,ImageTk
+
+#chọn ảnh
+def image():
+    global file_path
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        img = Image.open(file_path)
+        img = ImageTk.PhotoImage(img)
+        anhgoc = tk.Toplevel()
+        anh_goc = tk.Label(anhgoc, image = img)
+        anh_goc.pack()
+        anhgoc.mainloop()
+
+#1 nửa ảnh
+def Haft():
+    haft_img = cv2.imread(file_path)
+    h, w = haft_img.shape[:2]
+    half_width = w // 2
+    haft = haft_img[:, :half_width]
+    cv2.imshow('Haft_image', haft)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
