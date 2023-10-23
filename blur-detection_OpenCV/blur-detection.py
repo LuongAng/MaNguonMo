203793685397988375dbbcad94878d002c0e835e
LuongAng <134090680+LuongAng@users.noreply.github.com>
2023-10-23 23:30:35 +0700

    Add files via upload

--- blur-detection_OpenCV/blur-detection.py
+++ blur-detection_OpenCV/blur-detection.py
@@ -1,71 +1,43 @@
-import tkinter as tk
-from tkinter import filedialog
-from PIL import Image, ImageTk
+# import the necessary packages
+from imutils import paths
+import argparse
 import cv2
-import os
-
-def is_valid_image(file_path):
-    try:
-        image = Image.open(file_path)
-        return image.format in ['JPEG', 'PNG']  # Kiểm tra định dạng ảnh
-    except:
-        return False
-
+import sys
+ 
 def variance_of_laplacian(image):
-    return cv2.Laplacian(image, cv2.CV_64F).var()
+	# compute the Laplacian of the image and then return the focus
+	# measure, which is simply the variance of the Laplacian
+	return cv2.Laplacian(image, cv2.CV_64F).var()
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True,
+	help="path to input directory of images")
+ap.add_argument("-t", "--threshold", type=float, default=100.0,
+	help="focus measures that fall below this value will be considered 'blurry'")
+args = vars(ap.parse_args())
 
-def browse_image():
-    file_path = filedialog.askopenfilename()
-    entry_path.delete(0, tk.END)
-    entry_path.insert(0, file_path)
-    show_image(file_path)
-    check_button.config(state=tk.NORMAL)  # Khi đã chọn ảnh, bật nút kiểm tra
+# loop over the input images
+for imagePath in paths.list_images(args["images"]):
+	# load the image, convert it to grayscale, and compute the
+	# focus measure of the image using the Variance of Laplacian
+	# method
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	fm = variance_of_laplacian(gray)
 
-def show_image(file_path):
-    if file_path:
-        image = Image.open(file_path)
-        image.thumbnail((400, 400))
-        photo = ImageTk.PhotoImage(image)
+	if fm > args["threshold"]:
+		text = imagePath+" - Not Blurry: "+str(fm)
+		print(imagePath+" - Not Blurry: "+str(fm))
+ 
+	# if the focus measure is less than the supplied threshold,
+	# then the image should be considered "blurry"
+	if fm < args["threshold"]:
+		text = imagePath+" - Blurry: "+str(fm)
+		print(imagePath+" - Blurry: "+str(fm))
 
-        image_label.config(image=photo)
-        image_label.image = photo
 
-def check_blurriness():
-    file_path = entry_path.get()
-    if file_path:
-        if not is_valid_image(file_path):
-            result_label.config(text="Invalid image format. Please choose a JPEG or PNG image.")
-        else:
-            image = cv2.imread(file_path)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            fm = variance_of_laplacian(gray)
-
-            if fm > threshold:
-                result_label.config(text="Image is Not Blurry (Variance: {:.2f})".format(fm))
-            else:
-                result_label.config(text="Image is Blurry (Variance: {:.2f})".format(fm))
-
-root = tk.Tk()
-root.title("Image Blurriness Checker")
-
-label1 = tk.Label(root, text="Select an image:")
-label1.pack()
-
-entry_path = tk.Entry(root)
-entry_path.pack()
-
-browse_button = tk.Button(root, text="Browse", command=browse_image)
-browse_button.pack()
-
-check_button = tk.Button(root, text="Check Blurriness", command=check_blurriness, state=tk.DISABLED)
-check_button.pack()
-
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-image_label = tk.Label(root)
-image_label.pack()
-
-threshold = 500.0
-
-root.mainloop()
+#hướng dẫn sử dung:
+#1- vào terminal của pycharm
+#2- Chạy câu lệnh : "python blur-detection.py --images images"
+#3- Ngưỡng threshold mặc định là 100, bạn có thể tự xác định bằng cách thêm nhiều tham số như thế này bằng cách: "python blur-detection.py --images images --threshold 1000"
