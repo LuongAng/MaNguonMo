5120a6d48abbbe47b8625c2e8c09464f1c0d9044
LuongAng <luongngocanh18102@gmail.com>
2023-10-23 22:15:06 +0700

    baitap Nhom11 Buoi6

---
+++ chinhanh.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import Label, Canvas, Button, ttk
+from PIL import Image,ImageTk
