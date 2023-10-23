09fb35d11fe0feb2af9d539c2ecf074b3fb6cb7a
hieuajsk <137071973+hieuajsk@users.noreply.github.com>
2023-10-23 23:43:17 +0700

    Update blur-detection_Nhom11.py

--- blur-detection_OpenCV/blur-detection_Nhom11.py
+++ blur-detection_OpenCV/blur-detection_Nhom11.py
@@ -10,3 +10,19 @@ def is_valid_image(file_path):
         return image.format in ['JPEG', 'PNG']  # Kiểm tra định dạng ảnh
     except:
         return False
+
+def check_blurriness():
+    file_path = entry_path.get()
+    if file_path:
+        if not is_valid_image(file_path):
+            result_label.config(text="Invalid image format. Please choose a JPEG or PNG image.")
+        else:
+            image = cv2.imread(file_path)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            fm = variance_of_laplacian(gray)
+
+            if fm > threshold:
+                result_label.config(text="Image is Not Blurry (Variance: {:.2f})".format(fm))
+            else:
+                result_label.config(text="Image is Blurry (Variance: {:.2f})".format(fm))
+
