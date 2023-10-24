8e36bf4c69e19003f49132ae2fb7848e4e61f091
tuantruong2082002 <145415697+tuantruong2082002@users.noreply.github.com>
2023-10-24 08:02:57 +0700

    thiet ke giao dien chinh anh

--- chinhanh.py
+++ chinhanh.py
@@ -79,3 +79,42 @@ def C_G():
     canvas.get_tk_widget().pack()
 
     main.mainloop()
+
+root = tk.Tk()
+root.geometry("400x250") 
+root.title("IMAGE")
+
+open_button = ttk.Button(root, text = "Choose image", command = image)
+open_button.pack(pady = 10)
+
+haft_button = ttk.Button(root, text = "Haft", command = Haft)
+haft_button.pack()
+
+C_G_button = ttk.Button(root, text = "Color-Gray", command = C_G)
+C_G_button.pack(pady = 10)
+
+size_x = tk.StringVar()
+size_y = tk.StringVar()
+angle = tk.StringVar()
+
+rsz = tk.Label(root, text = "RESIZE:").place(x = 40, y = 135)
+
+x = tk.Label(root, text = "Size_X").place(x = 130, y = 120)
+
+y = tk.Label(root, text = "Size_Y").place(x = 130, y = 150)
+
+x_input_area = tk.Entry(root, textvariable = size_x, width = 5).place(x = 200, y = 120)
+
+y_input_area = tk.Entry(root, textvariable = size_y, width = 5).place(x = 200, y = 150)
+
+rz_button = ttk.Button(root, text = "Enter", command = resize).place(x = 280, y = 135)
+
+rtt = tk.Label(root, text = "ROTATION:").place(x = 40, y = 190)
+
+rtt_angle = tk.Label(root, text = "Angle").place(x = 130, y = 190)
+
+rtt_angle_input = tk.Entry(root, textvariable = angle, width = 5).place(x = 200, y = 190)
+
+rtt_button = ttk.Button(root, text = "Enter", command = rotation).place(x = 280, y = 190)
+
+root.mainloop()
