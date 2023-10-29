24b73474d7803f44d7d0093005628078a1a48ef8
LuongAng <134090680+LuongAng@users.noreply.github.com>
2023-10-29 23:53:46 +0700

    tao hàm chọn vùng ảnh chọn

--- bai2.py
+++ bai2.py
@@ -21,3 +21,22 @@ start_x, start_y, end_x, end_y = -1, -1, -1, -1
 is_drawing = False
 selected_region = None
 editing_completed = False
+def select_region(event, x, y, flags, param):
+    global start_x, start_y, end_x, end_y, is_drawing, img, selected_region
+
+    if not editing_completed:
+        if event == cv2.EVENT_LBUTTONDOWN:
+            start_x, start_y = x, y
+            is_drawing = True
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if is_drawing:
+                end_x, end_y = x, y
+        elif event == cv2.EVENT_LBUTTONUP:
+            is_drawing = False
+            end_x, end_y = x, y
+
+            # Vẽ hình chữ nhật để xác định vùng đã chọn
+            cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+
+            # Lưu vùng đã chọn
+            selected_region = img[start_y:end_y, start_x:end_x]
